3894f386a4009c41557293f9b3b5e9077ce86b6a
justinembawomye <jmbawomye@gmail.com>
2018-08-11 12:09:50 +0300

    learn oop

---
+++ classes.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.hobbies = ""
+
+#     def print_name(self, age, nickname):
+#         print(f"Justine is {age} years and her nickname is {nickname}")
+# Justine = Person("Jee", 20)  
+# print_name("Jee")  
+# # print(Justine.name) 
+
+
